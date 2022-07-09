d88ea5e47ee04f2fbbe7578a375a052376b39348
nerdboyq <princtonb@gmail.com>
2022-07-08 19:16:01 -0700

    updated to render the html template and offer the pi ip address for the browser

--- WebApp/chore_tracker_webapp.py
+++ WebApp/chore_tracker_webapp.py
@@ -1,12 +1,16 @@
 # Import Flask class
 from flask import Flask, render_template
  
- 
+# Webapp Theme colors : Yellow, Red, & Blue
+
 # Create Flask object
 app = Flask(__name__)
 
+# First page/screen seen when going to the site 
 @app.route('/')
 def hello_world():
     return render_template('homepage.html')
 
+# Runs the app when this file is executed, with the IP address
+# of the computer is running on as server for the webapp
 app.run(host="0.0.0.0", port=5000, debug=True)
